147c7de505e7ee5ca2974a82e792f4463aa96837
aniketsaste96 <90027213+aniketsaste96@users.noreply.github.com>
2021-11-18 19:26:42 +0530

    Add files via upload

---
+++ Day3/script1.js
@@ -0,0 +1,21 @@
+//Create your own resume data in JSON format
+
+let Name = [
+    {
+        "Name": "Aniket Krishna Saste",
+        "Education": "Bachelor in Engineering",
+        "UniversityName": "shivaji University,Kolhapur",
+        "Furthur Education": "Student of SIAC Mumbai 2018-2020",
+        "Graduation Year": 2016,
+        "workExperiance": "2years",
+        "Designation": "Quality Engineer",
+        "other Works involve in": "Trader,LLP at BESEA.pvt.lt",
+        "Hobbies": "'listening podcast','watching Documentaries'",
+        "sports": "Vollyball",
+
+
+
+
+
+    }
+]
